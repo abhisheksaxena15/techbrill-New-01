c05d419dbf1ed7c6b7addcd02d5fa49a5007b79a
Shivanshx4 <shivanshchowdhary04@gmail.com>
2025-08-02 17:45:51 +0530

    Delete techbrill-main directory

--- techbrill-main/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    // add other plugins here if needed
-  },
-};
-
-export default config;
